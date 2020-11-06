01b795b9b3e377346b1e670ab50e257e5c7a7600
Raymond <loki_fosil@hotmail.com>
2020-11-05 20:11:50 -0500

    Add basic search page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './content/assets/images/logo.png';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home"><img src={logo} alt="logo" />Solar Gators Inventory</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto pr-4">
+            <Nav.Link href="#home">Search</Nav.Link>
+            <Nav.Link href="#link">Manage</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Row className="align-items-center">>
+        <Col md={{offset: 4, span: 4}}>
+          <Form>
+            <Form.Group controlId="formSearch">
+              <Form.Control type="text" placeholder="Search" />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 }
